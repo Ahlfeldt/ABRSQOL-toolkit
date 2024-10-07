d0821bb5d9744bc7ea644ea29e1caf8724f3e003
Gabriel M Ahlfeldt <141059975+Ahlfeldt@users.noreply.github.com>
2024-10-07 16:42:18 +0200

    Update Example.m

--- MATLAB/Example.m
+++ MATLAB/Example.m
@@ -14,15 +14,15 @@ clc
 % See ABRSQOL.m for a detailed description of the syntax
 
 % Set the working directory
-cd('D:\Dropbox\GA_FB_DR_TS_MIGRATION\ABRSQOL-toolkit/MATLAB');
+    cd('C:\MATLABTEST');
 
 % Copy relevant files to working directory
 
     % Test data
         % Define the URL of the raw .mat file on GitHub
-        url = 'https://raw.githubusercontent.com/Ahlfeldt/ABRSQOL-toolkit/main/DATA/ABRSQOL-testdata.mat';
+        url = 'https://raw.githubusercontent.com/Ahlfeldt/ABRSQOL-toolkit/main/DATA/ABRSQOL-testdata.csv';
         % Define the destination file name, including the path
-        outputFile = 'ABRSQOL-testdata.mat';
+        outputFile = 'ABRSQOL-testdata.csv';
         % Download the file and save it to the specified directory
         websave(outputFile, url);
     % ABQRSQOL function
@@ -34,9 +34,41 @@ cd('D:\Dropbox\GA_FB_DR_TS_MIGRATION\ABRSQOL-toolkit/MATLAB');
         websave(outputFile, url);
 
 % Load the .mat file into the workspace
-load('ABRSQOL-testdata.mat');
+% load('ABRSQOL-testdata.mat');
+
+%% SET PARAMETER VALUES
+%%%%%%%%%%%%%%%%%%%%%%
+    alpha = 0.7;                    % income share on non-housing
+    beta = 0.3419;                  % share of alpha that is spent on tradable good                        
+    gamma = 3;                      % canonical value
+    xi = 5.5;                       % canonical value
+
+%% Note: Loading in all variables needed for inversion // 
+    % Data in Application originates from IEB datatset
+    % Note: should include the original dataset // Load your own dataset there
+
+%% For exemplary purposes use "testdata" instead
+    testdata = readmatrix("ABRSQOL-testdata.csv"); 
+    % Required inputs
+    llm_id = (1:141)';     % Local labour market identifier
+    w = testdata(:,2);     % Wages
+    p_H = testdata(:,3);   % Floor space price levels
+    P_t = testdata(:,4);   % Price levels of tradable goods
+    p_n = testdata(:,5);   % Price levels of nontradable services
+    L = testdata(:,6);     % Residence population
+    L_b = testdata(:,7);   % Hometown population    
 
 % Solve for region-specific QoL
-[A, O_total, test_agg] = ABRSQOL(alpha, beta, gamma, xi, w, p_H, P_t, p_n, L, L_b);
+    [A, O_total, test_agg] = ABRSQOL(alpha, beta, gamma, xi, w, p_H, P_t, p_n, L, L_b);
+
+% Save key outputs
+    % A and llm_id are both Jx1 vectors of the same size
+    J = length(A); % Replace with the actual size of A and llm_id if known
+    
+    % Create a table with the vectors A and llm_id
+    T = table(llm_id,A);
+    
+    % Write the table to a CSV file with headers
+    writetable(T, 'QoLoutput.csv');
 
 % Done
