40e3af5cf271d8aa0bb5270d038e90219ee5de04
maximylius <m.mylius@zeppelin.university.net>
2024-10-16 21:54:46 +0200

    removed unused argument from examples.

--- R/R/functions.R
+++ R/R/functions.R
@@ -56,7 +56,6 @@
 #' @examples
 #' ABRSQOL(
 #'  df=my_dataframe,
-#'  QoL_varname='A',
 #'  w = 'w', p_H = 'p_H', P_t = 'P_t', p_n = 'p_n', L = 'L', L_b = 'L_b',
 #'  alpha = 0.7, beta = 0.5, gamma = 3, xi = 5.5,
 #'  conv = 0.5, tolerance = 1e-10, maxiter = 1e4
@@ -71,8 +70,6 @@
 #' my_dataframe$quality_of_life = ABRSQOL(
 #'   # supply your dataset as a dataframe
 #'   df=my_dataframe,
-#'   # specify the name for the output variable
-#'   QoL_varname='qol_indicator',
 #'   # and specify the corresponding variable name for your dataset
 #'   w = 'wage',
 #'   p_H = 'floor_space_price',
@@ -136,12 +133,12 @@ ABRSQOL <- function(
   ) {
 
   # Extract key variables from input dataframe/matrix
-  L_b = matrix(data = as.vector(unlist(df[L_b])), ncol=length(L_b), byrow=FALSE)
-  L = matrix(data = as.vector(unlist(df[L])), ncol=length(L), byrow=FALSE)
-  w = matrix(data = as.vector(unlist(df[w])), ncol=length(w), byrow=FALSE)
-  P_t = df[[P_t]]
-  p_H = df[[p_H]]
-  p_n = df[[p_n]]
+  L_b = matrix(data = as.vector(unlist(df[L_b])), ncol=length(L_b), byrow=FALSE);
+  L = matrix(data = as.vector(unlist(df[L])), ncol=length(L), byrow=FALSE);
+  w = matrix(data = as.vector(unlist(df[w])), ncol=length(w), byrow=FALSE);
+  P_t = df[[P_t]];
+  p_H = df[[p_H]];
+  p_n = df[[p_n]];
   
 
   # if there are unequal number of rows (n_obs) among variables throw error
@@ -150,30 +147,30 @@ ABRSQOL <- function(
     )) != 1){
     stop("\nVariables do not have the same length:",
     "\nL_b: ",length(L_b), "\nL: ",length(L),"\nw: ",length(w),
-    "\nP_t: ",length(P_t),"\np_H: ",length(p_H),"\np_n: ",length(p_n))
+    "\nP_t: ",length(P_t),"\np_H: ",length(p_H),"\np_n: ",length(p_n));
   }
   # else save units of observation as J
-  J = length(L_b)
+  J = length(L_b);
 
   # if there are unequal number of dimensions throw an error
   if(length(unique(c(dim(L_b)[2],dim(L)[2],dim(w)[2]))) != 1){
     stop("\nVariables do not have the same dimension:",
-    "\nL_b: ",dim(L_b)[2], "\nL: ",dim(L)[2],"\nw:", dim(w)[2])
+    "\nL_b: ",dim(L_b)[2], "\nL: ",dim(L)[2],"\nw:", dim(w)[2]);
   }
   # else assign theta as the number of dimensions (mostly will be 1)
-  Theta = dim(L_b)[2]
+  Theta = dim(L_b)[2];
 
 
   ## Inversion
   
   # Adjust L_b to have same sum as L
-  L_bar = sum(L) # total number of workers in dataset
+  L_bar = sum(L); # total number of workers in dataset
   L_b_adjust <- L_bar / apply(L_b, 2, function(x) sum(x));
   L_b <- t(t(L_b) * L_b_adjust);
   
   # Express all variables in relative differences
   # Calculate relative employment, L_hat
-  L_hat <- sweep(L, 2, L[1,], `/`)
+  L_hat <- sweep(L, 2, L[1,], `/`);
   # Calculate relative wages, w_hat
   w_hat <- sweep(w, 2, w[1,], `/`);
   # Calculate relative price levels
@@ -182,8 +179,8 @@ ABRSQOL <- function(
   p_n_hat <- p_n / p_n[1];
 
   # Calculate aggregate price level
-  P_hat <- (P_t_hat ^(alpha *beta)) * (p_n_hat ^(alpha *(1-beta)))  * (p_H_hat ^(1-alpha));
-  P <- (P_t ^(alpha *beta))  * (p_n ^(alpha *(1-beta)))  * (p_H ^(1-alpha));
+  P_hat <- (P_t_hat ^(alpha * beta)) * (p_n_hat ^(alpha *(1-beta)))  * (p_H_hat ^(1-alpha));
+  P     <- (P_t     ^(alpha * beta)) * (p_n     ^(alpha *(1-beta)))  * (p_H     ^(1-alpha));
 
   # Relative Quality of life (A_hat)
   # Guess values relative QoL
@@ -191,12 +188,12 @@ ABRSQOL <- function(
   A <- A_hat;
   
   O_vector_total <- list(); # list to track convergence
-  O_total = max(100000, 2*tolerance) # Starting value for loop  
+  O_total = 100000; # Starting value for loop  
   count <- 1; # Counts the number of iterations
 
-  cat("\nBegin loop to solve for QoL...\n")
+  cat("\nBegin loop to solve for QoL...\n");
   while (O_total > tolerance && count <= maxiter){
-    cat("\r...itertion",count,", value of objective function:",O_total)
+    cat("\r...itertion",count,", value of objective function:",O_total);
     
     # (1) Calculate model-consistent aggregation shares, Psi_b
     nom <- (as.vector(A) * w  * as.vector(1/P)) ^(gamma);
@@ -230,9 +227,9 @@ ABRSQOL <- function(
   test_agg = sum(L_i)-L_bar; # should be zero!!
   test_i = L_i-L; # should be zero for all i !!#
   cat("\nOverall deviation:");
-  cat(str(test_agg))
+  cat(str(test_agg));
   cat("\nVector of deviations:");
-  cat(str(test_i,vec.len=20))
+  cat(str(test_i,vec.len=20));
   cat("\n------------------------------------------------------------------\n");
   cat("\n...QoL variable generated:\n")
   cat(str(A,vec.len=20))
