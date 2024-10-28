6e941833b9e6916147de2a830138102e3d0f2681
maximylius <m.mylius@zeppelin.university.net>
2024-10-28 14:36:58 +0100

    attempt to fix metadata by creating new set_matadata.py script

--- Python/ABRSQOL/__init__.py
+++ Python/ABRSQOL/__init__.py
@@ -1,9 +1,5 @@
-from importlib import metadata
-# this works, but usually people just write the name as a string here.
-# not 100% DRY, but it's not like the package name could ever change
-__title__ = __name__
-# if you're stuck on python 3.7 or older, importlib-metadata is a 
-# third-party package that can be used as a drop-in instead
-__version__ = metadata.version(__title__)
+from ..set_metadata import version
+__version__ = version
+
 from .invert_quality_of_life import *
 from .testdata import *
--- Python/setup.py
+++ Python/setup.py
@@ -1,6 +1,16 @@
 from setuptools import setup, find_packages
-
-name='ABRSQOL'
+from Python.set_metadata import (
+    name, 
+    version,
+    description,
+    # long_description,
+    # long_description_content_type,
+    url,
+    author,
+    author_email,
+    license,
+) 
+# name='ABRSQOL'
 
 extra_test = ['pytest>=4', 'pytest-cov>=2',]
 extra_dev = [*extra_test,'twine>=4.0.2',]
@@ -11,14 +21,14 @@ with open('./README.md', 'r') as f:
 
 setup(
     name=name,
-    version="0.1.0",
-    description='Numerical solution algorithm to invert a quality of life measure.',
+    version=version,
+    description=description,
     # long_description=long_description,
-    # long_description_content_type='text/markdown',
-    url='https://github.com/Ahlfeldt/ABRSQOL-toolkit/blob/main/Python',
-    author='Gabriel M Ahlfeldt',
-    author_email='g.ahlfeldt@hu-berlin.de',
-    license='MIT',
+    # long_description_content_type=long_description_content_type,
+    url=url,
+    author=author,
+    author_email=author_email,
+    license=license,
     install_requires=['numpy','pandas',],
     packages=[name],
     extras_require={
@@ -37,3 +47,32 @@ setup(
         'Programming Language :: Python :: 3',
     ],
 )
+
+# setup(
+#     name=name,
+#     version="0.1.0",
+#     description='Numerical solution algorithm to invert a quality of life measure.',
+#     # long_description=long_description,
+#     # long_description_content_type='text/markdown',
+#     url='https://github.com/Ahlfeldt/ABRSQOL-toolkit/blob/main/Python',
+#     author='Gabriel M Ahlfeldt',
+#     author_email='g.ahlfeldt@hu-berlin.de',
+#     license='MIT',
+#     install_requires=['numpy','pandas',],
+#     packages=[name],
+#     extras_require={
+#         'test': extra_test,
+#         'dev': extra_dev,
+#         'ci': extra_ci,
+#     },
+#     entry_points={
+#         # 'console_scripts': [
+#         #     'add=my_pip_package.math:cmd_add',
+#         # ],
+#     },
+#     classifiers=[
+
+#         'Programming Language :: Python',
+#         'Programming Language :: Python :: 3',
+#     ],
+# )
