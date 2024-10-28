74b3de2c28f6da60a996e062ceba67c1bf6e27be
maximylius <m.mylius@zeppelin.university.net>
2024-10-28 12:26:03 +0100

    hopefully solved chicken egg problem of not having required packages installed

--- Python/ABRSQOL/__init__.py
+++ Python/ABRSQOL/__init__.py
@@ -1,7 +1,2 @@
-__copyright__ = "Gabriel M Ahlfeldt"
-__author__ = "maximylius"
-__license__ = "MIT"
-__version__ = '1.0'
-
 from .invert_quality_of_life import *
 from .testdata import *
--- Python/setup.py
+++ Python/setup.py
@@ -1,7 +1,5 @@
 from setuptools import setup, find_packages
 
-from ABRSQOL import __version__
-
 
 extra_test = [
     'pytest>=4',
@@ -9,6 +7,7 @@ extra_test = [
 ]
 extra_dev = [
     *extra_test,
+    'twine>=4.0.2'
 ]
 
 extra_ci = [
@@ -16,22 +15,29 @@ extra_ci = [
     'python-coveralls',
 ]
 
-setup(
-    name='ABRSQOL',
-    version=__version__,
-    description='Numerical solution algorithm to invert a quality of life measure.',
+name='ABRSQOL'
 
+with open('./README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name=name,
+    version="0.1.0",
+    description='Numerical solution algorithm to invert a quality of life measure.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/Ahlfeldt/ABRSQOL-toolkit/blob/main/Python',
     author='Gabriel M Ahlfeldt',
     author_email='g.ahlfeldt@hu-berlin.de',
-    
+    license='MIT',
     setup_requires=["numpy"],
-    install_requires=[
-        'numpy', 'pandas', 
+    install_requires=['numpy', 
+                      'pandas', 
     ],
 
-    packages=find_packages(exclude=['tests', 'tests.*']),
-    
+    packages=[name],
+    # packages=find_packages(where='app'),
+    # package_dir={"":"app"}
     extras_require={
         'test': extra_test,
         'dev': extra_dev,
@@ -39,13 +45,12 @@ setup(
     },
 
     entry_points={
-        'console_scripts': [
-            'add=my_pip_package.math:cmd_add',
-        ],
+        # 'console_scripts': [
+        #     'add=my_pip_package.math:cmd_add',
+        # ],
     },
 
     classifiers=[
-        'Intended Audience :: Researchers',
 
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
